ab601d7c5eb6fd9f6b4f4748da73f370a76e244e
jaycee808 <jamie.clark@live.co.uk>
2023-02-20 13:35:30 +0000

    added variables for five day weather data

--- assests/js/script.js
+++ assests/js/script.js
@@ -18,7 +18,8 @@ function searchWeather(event) {
         method: "GET"
     }).then(function(response) {
         console.log(response);
-    // response data - description, weather icon, temperature, humidity, wind speed
+
+    // response data - name, description, weather icon, temperature, humidity, wind speed
     var cityName = response.name;
     console.log(cityName);
     var temperature = response.main.temp;
@@ -35,7 +36,6 @@ function searchWeather(event) {
     console.log(latitude);
     var longitude = response.coord.lon.toFixed(2);
     console.log(longitude);
-
 });
 }
 
@@ -46,7 +46,6 @@ function forecastData(event) {
     var userInput = $("#search-input").val(); 
     console.log(userInput);
         
-    
     // five day forecast data
         var forecastURL = "https://api.openweathermap.org/data/2.5/forecast?q=" + userInput + "&appid=" + apiKey + "&units=metric";
 
@@ -59,26 +58,71 @@ function forecastData(event) {
         }).then(function(response) {
             console.log(response);
 
-    
-    var forecastDayOne = response.list[8].dt_txt.split(' ')[0];
-    console.log(forecastDayOne);
+        // Day 1 Variables
+        var forecastDayOneDate = response.list[8].dt_txt.split(' ')[0];
+        console.log(forecastDayOneDate);
+        var descriptionDayOne = response.list[8].weather[0].description;
+        var iconDayOne = response.list[8].weather[0].icon;
+        var temperatureDayOne = response.list[8].main.temp;
+        var humidityDayOne = response.list[8].main.humidity;
+        console.log(descriptionDayOne);
+        console.log(iconDayOne);
+        console.log(temperatureDayOne);
+        console.log(humidityDayOne);
 
-    var forecastDayTwo = response.list[16].dt_txt.split(' ')[0];
-    console.log(forecastDayTwo);
+        // Day 2 Variables
+        var forecastDayTwoDate = response.list[16].dt_txt.split(' ')[0];
+        console.log(forecastDayTwoDate);
 
-    var forecastDayThree = response.list[24].dt_txt.split(' ')[0];
-    console.log(forecastDayThree);
+        var descriptionDayTwo = response.list[16].weather[0].description;
+        var iconDayTwo = response.list[16].weather[0].icon;
+        var temperatureDayTwo = response.list[16].main.temp;
+        var humidityDayTwo = response.list[16].main.humidity;
+        console.log(descriptionDayTwo);
+        console.log(iconDayTwo);
+        console.log(temperatureDayTwo);
+        console.log(humidityDayTwo);
 
-    var forecastDayFour = response.list[32].dt_txt.split(' ')[0];
-    console.log(forecastDayFour);
+        // Day 3 Variables
+        var forecastDayThreeDate = response.list[24].dt_txt.split(' ')[0];
+        console.log(forecastDayThreeDate);
 
-    var forecastDayFive = response.list[39].dt_txt.split(' ')[0];
-    console.log(forecastDayFive);
+        var descriptionDayThree = response.list[24].weather[0].description;
+        var iconDayThree = response.list[24].weather[0].icon;
+        var temperatureDayThree = response.list[24].main.temp;
+        var humidityDayThree = response.list[24].main.humidity;
+        console.log(descriptionDayThree);
+        console.log(iconDayThree);
+        console.log(temperatureDayThree);
+        console.log(humidityDayThree);
 
+        // Day 4 Variables
+        var forecastDayFourDate = response.list[32].dt_txt.split(' ')[0];
+        console.log(forecastDayFourDate);
 
+        var descriptionDayFour = response.list[32].weather[0].description;
+        var iconDayFour = response.list[32].weather[0].icon;
+        var temperatureDayFour = response.list[32].main.temp;
+        var humidityDayFour = response.list[32].main.humidity;
+        console.log(descriptionDayFour);
+        console.log(iconDayFour);
+        console.log(temperatureDayFour);
+        console.log(humidityDayFour);
+
+        // Day 5 Variables
+        var forecastDayFiveDate = response.list[39].dt_txt.split(' ')[0];
+        console.log(forecastDayFiveDate);
+
+        var descriptionDayFive = response.list[32].weather[0].description;
+        var iconDayFive = response.list[32].weather[0].icon;
+        var temperatureDayFive = response.list[32].main.temp;
+        var humidityDayFive = response.list[32].main.humidity;
+        console.log(descriptionDayFive);
+        console.log(iconDayFive);
+        console.log(temperatureDayFive);
+        console.log(humidityDayFive);
 });
 }
-
 
 // event listener for search button
 searchForm.on('submit', searchWeather);
